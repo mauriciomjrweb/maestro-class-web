6a3170992be2af5c86e8a67ddbb38e990e23c429
mauriciomjrweb <mauriciojr.web@gmail.com>
2022-04-05 17:31:07 -0300

    Primeiro Commit

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Curso;
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+            $cursos = Curso::latest()->paginate(5);
+        return view('cursos.index', compact('cursos'))
+                        ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('cursos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'nome' => 'required'
+        ]);
+        Curso::create($request->all());
+
+        return redirect()->route('cursos.index')
+                        ->with('success', 'Curso inserido com sucesso');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Curso  $curso
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Curso $curso)
+    {
+        //
+        return view('cursos.view', compact('curso'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Curso  $curso
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Curso $curso)
+    {
+        //
+        return view('cursos.edit', compact('curso'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Curso  $curso
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Curso $curso)
+    {
+        //
+        $request->validate(['nome' => 'required']);
+
+        Curso::update($request->all());
+        return redirect()->route('cursos.index')
+                        ->with('success', 'Curso created successfully.');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Curso  $curso
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Curso $curso)
+    {
+        //
+            $curso->delete();
+
+        return redirect()->route('cursos.index')
+                        ->with('success', 'Curso deleted successfully');
+    }
+}
